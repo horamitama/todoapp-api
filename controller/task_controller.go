5dab225429665f7c4d12b8ed428573196ace8eb7
ほらみたま <horamitama@hora.local>
2024-09-01 16:17:35 +0900

    impl task controller and repository

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"todoapp-api/entity"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetTasks(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -8,8 +13,11 @@ func GetTasks(c *gin.Context) {
 	})
 }
 
-// r.GET("/", func(c *gin.Context) {
-// 	c.JSON(200, gin.H{
-// 		"message": "pong",
-// 	})
-// })
+func CreateTask(c *gin.Context) {
+	task := entity.Task{}
+	err := c.Bind(&task)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+	c.JSON(200, gin.H{"task": task})
+}
---
+++ repository/task_repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import (
+	"todoapp-api/entity"
+)
+
+func Create(task *entity.Task) {
+	// db := db.NewDB()
+	// result := db.Where()
+}

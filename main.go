10b9679c0c46de58b102e5aa72fbe009c2ae96b3
horamitama <horahora0828@gmail.com>
2024-09-22 11:08:40 +0900

    impl list tasks

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	r.POST("/signin", controller.SignIn)
 	r.POST("/signout", controller.SignOut)
 	r.POST("/task", controller.CreateTask)
-	r.GET("/task", controller.GetTasks)
+	r.GET("/tasks", controller.GetTasks)
 	r.Run()
 }

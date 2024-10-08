597c187479c47e0982d08d1d8ba33db9fb282417
horamitama <horahora0828@gmail.com>
2024-10-08 16:06:21 +0900

    impl: create and list task

---
+++ usecase/task.go
@@ -0,0 +1,51 @@
+package usecase
+
+import (
+	"todoapp-api/model"
+	"todoapp-api/repository"
+)
+
+type TaskUsecase struct {
+	tr repository.TaskRepositoryInterfase
+}
+
+type TaskUsecaseInterface interface {
+	Create(*model.Task) error
+	List(*model.Task) (*[]model.Task, error)
+	Get(*model.Task) error
+	Update(*model.Task) error
+	Delete(*model.Task) error
+}
+
+func NewTaskUsecase(tr repository.TaskRepositoryInterfase) TaskUsecaseInterface {
+	return &TaskUsecase{tr}
+}
+
+func (tu *TaskUsecase) Create(task *model.Task) error {
+	err := tu.tr.Create(task)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tu *TaskUsecase) List(task *model.Task) (*[]model.Task, error) {
+	var storedTasks []model.Task
+	err := tu.tr.FindAllByUserId(&storedTasks, task.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &storedTasks, nil
+}
+
+func (tu *TaskUsecase) Get(task *model.Task) error {
+	return nil
+}
+
+func (tu *TaskUsecase) Update(task *model.Task) error {
+	return nil
+}
+
+func (tu *TaskUsecase) Delete(task *model.Task) error {
+	return nil
+}
